6d39c4fd07eb9833e8b392502d3766044df9b688
Aziz Yakubjanov <yakubjanov0@hotmail.com>
2017-01-04 23:21:15 +0000

    init

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,28 +1,56 @@
+function sortByName(a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+}
+
 $(function() {
+  let pokemonRequest = prompt('What pokemon do you want to learn about?');
+  
   // Step 0: Set your assigned pokemon to the "pokemon" variable! I have put in Bulbasaur for an example
-  var pokemon = charmander;
+  var pokemon = window[pokemonRequest.toLowerCase()];
 
   // STEP 1: Uncomment the next line and make sure that the pokemon object prints to the console!
-  //   e.g. console.log(pokemon)
+  console.log(pokemon)
 
   // STEP 2: Use jQuery to add information to the empty fields. The first few have been done for you.
   // Note that some of these fields may not exist for your pokemon. 
   
   $(".pokemon-name").append(pokemon.name);
   $(".pokemon-evolves").append(pokemon.evolutions[0].to);
-  $(".pokemon-ev-level").append("?") // Replace the ?
-  // TODO: Add evolution level here
-  // TODO: Add speed here
-  // TODO: Add attack here
-  // TODO: Add defense here
+  $(".pokemon-ev-level").append(pokemon.evolutions[0].level);
+  $(".pokemon-spd").append(pokemon.speed);
+  $(".pokemon-attack").append(pokemon.attack);
+  $(".pokemon-defense").append(pokemon.defense);
   
   // TODO: Add ability names here to the <ul> with class .ability-names
-  
+  pokemon.abilities.forEach((val, index) => {
+    let listItem = $('<li/>').text(val.name);
+    $('.ability-names').append(listItem);
+  });
   // TODO: Add egg group names here to a <ul> that you created
-
-  // TODO: Add move names in alphabetical order here to the existing <ul>
+  pokemon.egg_groups.forEach((val, index) => {
+    let listItem = $('<li/>').text(val.name);
+    $('.egg-groups').append(listItem);
+  });
+  pokemon.moves = pokemon.moves.sort(sortByName);
+  pokemon.moves.forEach((val, index) => {
+    let listItem = $('<li/>').text(val.name);
+    $('.moves-list').append(listItem);
+  });
   // TODO: Add ONLY level up move names to the <ul> you created
-
+  pokemon.moves.filter(x => x.learn_type === 'level up').forEach((val, index) => {
+    let listItem = $('<li/>').text(val.name);
+    $('.moves-list-level-up').append(listItem);
+  });
   // Now... take a deep breath and go back to your move lists.
   // TODO: Find a way to use variables to make your code more readable!
 
